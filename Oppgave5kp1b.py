93d0b6bedeca7baccf1e67564ef1fecf55710a32
kingCrubi <camilohrubilar@gmail.com>
2023-09-19 12:46:16 +0200

    Update Oppgave5kp1b.py

--- Oppgave5kp1b.py
+++ Oppgave5kp1b.py
@@ -2,3 +2,8 @@ UserInput= (str(input("Skriv inn Grader i F eller C: ")))
 
 Benevning= UserInput[-1:]
 print(Benevning)
+if Benevning== ("C"):
+    print(Benevning)
+else:
+    print(Benevning)
+    
