f1192971777532ab07bcaeec6f54e7213d681d5e
kingCrubi <camilohrubilar@gmail.com>
2023-09-19 12:56:33 +0200

    Update Kopi.py

---
+++ Kopi.py
@@ -0,0 +1,9 @@
+UserInput= (str(input("Skriv inn Grader i F eller C: ")))
+
+Benevning= UserInput[-1:]
+print(Benevning)
+if Benevning== ("C"):
+    print(Benevning)
+else:
+    print(Benevning)
+    
