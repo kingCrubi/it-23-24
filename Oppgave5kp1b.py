ddcf54a96ee64f084913c58adaf32fc1431755a0
Jaran Romnes <jarpen205@gmail.com>
2023-09-19 13:16:31 +0200

    Update Oppgave5kp1b.py
    Nå har benevning f eller c og userinput er bare tallet

--- Oppgave5kp1b.py
+++ Oppgave5kp1b.py
@@ -1,7 +1,10 @@
 UserInput= (str(input("Skriv inn Grader i F eller C: ")))
 
-Benevning= UserInput[-1:]
+Benevning = UserInput[-1:]
+UserInput = int(UserInput[:len(UserInput)-1])
+
 print(Benevning)
+print(UserInput)
 if Benevning== ("C"):
     print(Benevning)
 else:
